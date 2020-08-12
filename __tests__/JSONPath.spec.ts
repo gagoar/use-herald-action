504e85fd799f82a8fdeda0577c34e2234934f477
Gago <xeroice@gmail.com>
2020-08-12 10:19:40 -0700

    :gun:

--- __tests__/JSONPath.spec.ts
+++ __tests__/JSONPath.spec.ts
@@ -1,11 +1,11 @@
 import { loadJSONFile } from '../src/util/loadJSONFile';
 import { JSONPath } from '@astronautlabs/jsonpath';
-
+import { env } from '../src/environment';
 describe('JsonPath', () => {
   it('should match excluding user but matching title', () => {
     const pattern = "$[?(@.body.match(/Issue Reference.*: #[0-9]{3}/) && @.user.login != 'renovate-bot')]";
 
-    const event = loadJSONFile<Event>(process.env.GITHUB_EVENT_PATH);
+    const event = loadJSONFile<Event>(env.GITHUB_EVENT_PATH);
     const result = JSONPath.query(event, pattern);
 
     expect(result).toMatchInlineSnapshot(`
