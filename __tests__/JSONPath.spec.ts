786fea5fac516e689726e44553e1dbbdd2307164
Gago <xeroice@gmail.com>
2020-08-12 10:36:00 -0700

    test will show what is going on

--- __tests__/JSONPath.spec.ts
+++ __tests__/JSONPath.spec.ts
@@ -1,13 +1,20 @@
 import { loadJSONFile } from '../src/util/loadJSONFile';
 import { JSONPath } from '@astronautlabs/jsonpath';
 import { env } from '../src/environment';
+import { Event } from '../src/util/constants';
 describe('JsonPath', () => {
   it('should match excluding user but matching title', () => {
-    const pattern = "$[?(@.body.match(/Issue Reference.*: #[0-9]{3}/) && @.user.login != 'renovate-bot')]";
+    const pattern = "$[?(@.user.login != 'renovate-bot')].user.login";
 
     const event = loadJSONFile<Event>(env.GITHUB_EVENT_PATH);
+    console.warn(event.pull_request.body);
     const result = JSONPath.query(event, pattern);
 
+    expect(env.GITHUB_EVENT_PATH).toMatchInlineSnapshot('__mocks__/event.json');
+    expect(event.pull_request.body).toMatchInlineSnapshot(`
+      "This is a pretty simple change that we need to pull into master
+       This is a pretty simple change that we need to pull into master (**Issue Reference**: #1234) "
+    `);
     expect(result).toMatchInlineSnapshot(`
       Array [
         "Update the README with new information.",
