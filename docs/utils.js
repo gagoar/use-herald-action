20b3f30e02970f4d67e7184fdf18a2086ec621c5
Simon Liu <cyamonide@gmail.com>
2020-09-20 01:26:48 -0400

    Finish flow

--- docs/utils.js
+++ docs/utils.js
@@ -4,7 +4,7 @@
  * @param  {String} url The URL
  * @return {Object}     The URL parameters
  */
-var getParams = function (url) {
+const getParams = function (url) {
   var params = {};
   var parser = document.createElement('a');
   parser.href = url;
@@ -15,4 +15,32 @@ var getParams = function (url) {
     params[pair[0]] = decodeURIComponent(pair[1]);
   }
   return params;
+};
+
+const copy = function (id, parentId) {
+  const textToCopy = document.getElementById(id).innerHTML;
+
+  var myTemporaryInputElement = document.createElement('input');
+  myTemporaryInputElement.type = 'text';
+  myTemporaryInputElement.value = textToCopy;
+
+  document.body.appendChild(myTemporaryInputElement);
+
+  myTemporaryInputElement.select();
+  document.execCommand('Copy');
+
+  document.body.removeChild(myTemporaryInputElement);
+
+  const badge = document.querySelectorAll(`#${parentId} .badge`)[0];
+
+  badge.classList.remove('badge-info');
+  badge.classList.add('badge-success');
+  const oldText = badge.innerHTML;
+  badge.innerHTML = 'Copied to clipboard!';
+
+  setTimeout(() => {
+    badge.classList.remove('badge-success');
+    badge.classList.add('badge-info');
+    badge.innerHTML = oldText;
+  }, 2000);
 };
